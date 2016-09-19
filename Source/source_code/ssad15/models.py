df76bbc9cf97ba4cbce92d22b738df8b516e3354
kritz3112 <kritika.agrawal@research.iiit.ac.in>
2016-09-20 00:10:38 +0530

    Adding models

---
+++ Source/source_code/ssad15/models.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+
+class zone(models.Model):
+    total_bundles=models.IntegerField(default=10)
+    bundles_used=models.IntegerField(default=0)
+    cost=models.DecimalField(default=0,max_digits=10,decimal_places=2)
+    top_left_coordinate=models.DecimalField(max_digits=5,decimal_places=2)
+    top_right_coordinate=models.DecimalField(max_digits=5,decimal_places=2)
+    bottom_left_coordinate=models.DecimalField(max_digits=5,decimal_places=2)
+    bottom_right_coordinate=models.DecimalField(max_digits=5,decimal_places=2)
+
+class slot(models.Model):
+    zone_id = models.ForeignKey(zone, on_delete=models.CASCADE)
+    slot_no = models.IntegerField(default=0)
+
+class advertisement(models.Model):
+    slot_id = models.ForeignKey(slot, on_delete=models.CASCADE)
+    upload = models.FileField(upload_to='uploads/')
+
+
+#to store  device ids of configured devices
+#to be decided if username of device will be alphanumeric or numeric string
+class devices(models.Model):
+    username=models.CharField(max_length=10,unique=True)
